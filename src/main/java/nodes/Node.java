f75b91b7976984c14c278a6380ad44ac7b564ebe
unalo <unalomtshokotsha@gmail.com>
2020-02-17 14:48:13 +0200

    fixed error on node

--- src/main/java/nodes/Node.java
+++ src/main/java/nodes/Node.java
@@ -1,6 +1,6 @@
 package nodes;
 
-public class Node extends CreatingNode implements Comparable <CreatingNode> {
+public class Node extends CreatingNode  {
     public Node(int id, int parentID, String label) {
         super(id, parentID, label);
     }
